9d7b035f3daa729879a60d1b2ae4de8c19263847
Radhakrishnan Meiappan <Radhakrishnan.M@asg.com>
2020-02-17 12:58:58 +0530

    added keygen step

--- jenkins-job-dsl/terraform-nodeapp-create-job-shell.sh
+++ jenkins-job-dsl/terraform-nodeapp-create-job-shell.sh
@@ -2,6 +2,5 @@ export AWS_ACCESS_KEY_ID="$aws_access_keyId"
 export AWS_SECRET_ACCESS_KEY="$aws_secret_accessKey"
 terraform init
 aws s3 cp s3://terraform-jenkins-j347fskj45098/amivar.tf amivar.tf
-touch mykey
-touch mykey.pub
+ssh-keygen -f mykey -N ''
 terraform apply -auto-approve

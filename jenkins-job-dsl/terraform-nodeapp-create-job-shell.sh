c43b220c7c531f00c6bd89c7005655b86a47132d
Radhakrishnan Meiappan <Radhakrishnan.M@asg.com>
2020-02-17 13:36:09 +0530

    adding input for overwrite y'

--- jenkins-job-dsl/terraform-nodeapp-create-job-shell.sh
+++ jenkins-job-dsl/terraform-nodeapp-create-job-shell.sh
@@ -2,5 +2,5 @@ export AWS_ACCESS_KEY_ID="$aws_access_keyId"
 export AWS_SECRET_ACCESS_KEY="$aws_secret_accessKey"
 terraform init
 aws s3 cp s3://terraform-jenkins-j347fskj45098/amivar.tf amivar.tf
-ssh-keygen -f mynodekey -N ''
+ssh-keygen -f mynodekey -N '' <<< y
 terraform apply -auto-approve
